33270abb8a68c53a8bbcc5ac1c25138ed4da11cd
Yan <41467560+yanissohappy@users.noreply.github.com>
2022-01-21 20:05:05 +0800

    Complete this work

--- sketch.js
+++ sketch.js
@@ -1,32 +1,46 @@
-//declare variables
-var moon_X = 60;
-var moon_Y = 50;
-var sunset = 0;
-var day_or_night = 0; 
+var moon_X = 120;
+var moon_Y = 100;
+var moon;
+var moon_diameter = 60;
+var normal_moon_ctrl = true;
+// 煙火控制器
 var fireworks = []; // 放煙火粒子的地方，可以支援多個煙火
+var firework_control = true;
+
 var gravity; // 煙火掉下來需要重力的加持
-var colorsBlue = "04080f-507dbc-a1c6ea-bbd1ea-dae3e5".split("-").map(a=>"#"+a) //給定顏色
 var blur_ctrl = 0;
 var blur_ctrl_flag = false; // false 代表"從 0 往上數, true 代表從最高往下數"的判斷標籤
 var stars = [];
-var star_num = 30;
+var star_num = 150;
 var clouds = [];
 var cloud_object;
 var cloud_init_y = 0;
 var canvasSize;
 var shooting_stars = [];
-
-var lanterns = []
-
+// 燈籠
+var lanterns = [];
+var sai_lantern;
+var normal_sai_lantern_ctrl = true;
+// 攤販
+var vendors = [];
 // 放音樂
-var audioCtx = null;
-var analysers = [];
-
-// 畫山
-var cFurther, cCloser, cMist;
+var musicFiles = [];
+var audioCtx;
+var audio;
+var audioSrc;
+var analyser;
+var bufferLength;
+var dataArray;
+var background_music;
+var initial_volume = 0.25;
+// 山
+var cFurther, cCloser;
 var a, b, c, d, e;
 
 function preload() {
+    windowWidth = windowHeight;
+    moon = new Moon(moon_X, moon_Y);
+  
     // preload() runs once
     cloud_object1 = new Cloud('cloud1.png');
     clouds.push(cloud_object1);
@@ -41,13 +55,42 @@ function preload() {
     cloud_init_y = int(random(0, 100));
   
     // 燈籠 (目前還都是用燈泡測試)
-    lantern_object1 = new Lantern('lightbulb.png');
-    lanterns.push(lantern_object1);
-    lantern_object2 = new Lantern('lightbulb1.png');
-    lanterns.push(lantern_object2);
+    var offset = 0.012;
+    lanterns.push(new Lantern('blue-1.png', int(windowWidth*(0.125 * 0 + offset)), int(windowHeight*0.428)));
+    lanterns.push(new Lantern('blue-2.png', int(windowWidth*(0.125 * 0 + offset)), int(windowHeight*0.428)));
+  
+    lanterns.push(new Lantern('green-1.png', int(windowWidth*(0.125 * 1 + offset)), int(windowHeight*0.463)));
+    lanterns.push(new Lantern('green-2.png', int(windowWidth*(0.125 * 1 + offset)), int(windowHeight*0.463)));
+  
+    lanterns.push(new Lantern('orange-1.png', int(windowWidth*(0.125 * 2 + offset)), int(windowHeight*0.488)));
+    lanterns.push(new Lantern('orange-2.png', int(windowWidth*(0.125 * 2 + offset)), int(windowHeight*0.488)));
+  
+    lanterns.push(new Lantern('purple-1.png', int(windowWidth*(0.125 * 3 + offset)), int(windowHeight*0.509)));
+    lanterns.push(new Lantern('purple-2.png', int(windowWidth*(0.125 * 3 + offset)), int(windowHeight*0.509)));
+  
+    lanterns.push(new Lantern('red-1.png', int(windowWidth*(0.125 * 4 + offset)), int(windowHeight*0.526)));
+    lanterns.push(new Lantern('red-2.png', int(windowWidth*(0.125 * 4 + offset)), int(windowHeight*0.526)));
+  
+    lanterns.push(new Lantern('yellow-1.png', int(windowWidth*(0.125 * 5 + offset)), int(windowHeight*0.540)));
+    lanterns.push(new Lantern('yellow-2.png', int(windowWidth*(0.125 * 5 + offset)), int(windowHeight*0.540)));
+  
+    lanterns.push(new Lantern('white-1.png', int(windowWidth*(0.125 * 6 + offset)), int(windowHeight*0.552)));
+    lanterns.push(new Lantern('white-2.png', int(windowWidth*(0.125 * 6 + offset)), int(windowHeight*0.552)));
+  
+    // lanterns.push(new Lantern('sai-1.png', int(windowWidth*(0.125 * 7 + offset)), int(windowHeight*0.554)));
+    // lanterns.push(new Lantern('sai-2.png', int(windowWidth*(0.125 * 7 + offset)), int(windowHeight*0.554)));
+    sai_lantern_1 = new Lantern('sai-1.png', int(windowWidth*(0.125 * 7 + offset)), int(windowHeight*0.554));
+    sai_lantern_2 = new Lantern('sai-2.png', int(windowWidth*(0.125 * 7 + offset)), int(windowHeight*0.554));
+  
+  
+    // 攤商
+    vendors.push(new Vendor('vendor-1.png', 0, windowHeight - int(windowHeight / 3)));
+    vendors.push(new Vendor('vendor-2.png', int(windowWidth * (1/3) + 1), windowHeight - int(windowHeight / 3)));
+    vendors.push(new Vendor('vendor-3.png', int(windowWidth * (2/3) + 1), windowHeight - int(windowHeight / 3)));
   
     // 先把音樂灌好
-    // GetAudio();
+    background_music = loadSound("firework_festival.mp3");
+    background_music.setVolume(initial_volume);
 }
 
 function setup(){
@@ -61,93 +104,183 @@ function setup(){
     stroke(255);
     strokeWeight(4);
     background(0);
-	frameRate(40);
-    // randomSeed(99);
-    // console.log(parseInt(random(0, 2)));
+	frameRate(60);
     createStars();
     noStroke();
   
     cFurther = color(255, 255, 255);
     cCloser = color(0, 0, 0);
-    cMist = color(255,255,255);
   
     a = random(-width/2, width/2);  //random discrepancy between the sin waves
-    b = random(-width/2, width/2);  //random discrepancy between the sin waves  
+    b = random(-width/2, width/2);  //random discrepancy between the sin waves 
     c = random(2, 4);  //random amplitude for the second sin wave
     d = random(40, 50);  //noise function amplitude
     e = random(-width/2, width/2);
+  
+    background_music.loop();
+  
+    analyzer = new p5.Amplitude();
+    analyzer.setInput(background_music);
 }
+
 function draw(){
 	noStroke();
     colorMode(RGB);
-    eveningBackground();
-    if (random(1) < 0.015 && fireworks.length < 4) {
-        fireworks.push(new Firework());
+    if(normal_moon_ctrl == true){
+      eveningBackground();
+    }else{
+      nightBackground();
     }
-    // fireworks.push(new Firework());
-    for (var i = fireworks.length-1; i >= 0; i--) {
-      fireworks[i].update();
-      fireworks[i].show();
-      if (fireworks[i].done())
-        fireworks.splice(i, 1);
+    drawStars();
+    moon.draw();
+    if(firework_control == true){
+      if (random(1) < 0.03 && fireworks.length < 3) {
+          fireworks.push(new Firework());
+      }
+      for (var i = fireworks.length-1; i >= 0; i--) {
+        fireworks[i].update();
+        fireworks[i].show();
+        if (fireworks[i].done())
+          fireworks.splice(i, 1);
+      }
     }
-      // fireworks[0].update();
-      // fireworks[0].show();
-      // if (fireworks[0].exploded == true){
-      //     Pd.send('hit', ['bang']);
-      // }
-      // if (fireworks[0].done()){
-      //     fireworks.splice(0, 1);
-      // }
   
     // make a shooting star  
-//     if(shooting_stars.length == 0){
-//         shooting_stars.push(new ShootingStar());
-//         console.log(shooting_stars.length);        
-//     }
+    // if(shooting_stars.length == 0){
+    //     shooting_stars.push(new ShootingStar());
+    //     if(shooting_stars.length > 0){
+    //       starFly(0);
+    //     }
+    // }
   
-//     starFly(0);
+    mountains(cCloser, cFurther);
 
-    // blendMode(SCREEN)
-    noStroke();
-    drawStars();
-    moon(moon_X, moon_Y);
-	// mountain();
-    // ground();
-    // starFly();
-    mountains(cCloser, cFurther, cMist);
-    // building();
-    // drawCloud();
     for (var i = clouds.length-1; i >= 0; i--) {
         clouds[i].draw();
     }
+  
+    var rms = analyzer.getLevel();
+  
+    // 聲音震幅
+    _range = 1 * initial_volume;
+    _range *= 0.2948;
+    _range /= 8;
     
-    // 燈籠  
-    // if(frameCount % 2 == 0){
-    //   lanterns[0].draw();
-    // }else{
-    //   lanterns[1].draw();
-    // }
+    // 燈籠
+    // 奇數都是暗的
+    if(rms <= _range){
+      lanterns[1].draw(); // 完全不亮
+      lanterns[3].draw();
+      lanterns[5].draw();
+      lanterns[7].draw();
+      lanterns[9].draw();
+      lanterns[11].draw();
+      lanterns[13].draw();
+    }else if(rms >= _range && rms < _range * 2){
+      lanterns[0].draw(); // 亮第一顆
+      lanterns[3].draw();
+      lanterns[5].draw();
+      lanterns[7].draw();
+      lanterns[9].draw();
+      lanterns[11].draw();
+      lanterns[13].draw();
+    }else if(rms >= _range * 2 && rms < _range * 3){
+      lanterns[0].draw(); // 亮第一顆
+      lanterns[2].draw(); // 亮第二顆
+      lanterns[5].draw();
+      lanterns[7].draw();
+      lanterns[9].draw();
+      lanterns[11].draw();
+      lanterns[13].draw();
+    }else if(rms >= _range * 3 && rms < _range * 4){
+      lanterns[0].draw(); // 亮第一顆
+      lanterns[2].draw(); // 亮第二顆
+      lanterns[4].draw(); // 亮第三顆
+      lanterns[7].draw();
+      lanterns[9].draw();
+      lanterns[11].draw();
+      lanterns[13].draw();
+    }else if(rms >= _range * 4 && rms < _range * 5){
+      lanterns[0].draw(); // 亮第一顆
+      lanterns[2].draw(); // 亮第二顆
+      lanterns[4].draw(); // 亮第三顆
+      lanterns[6].draw(); // 亮第四顆
+      lanterns[9].draw();
+      lanterns[11].draw();
+      lanterns[13].draw();
+    }else if(rms >= _range * 5 && rms < _range * 6){
+      lanterns[0].draw(); // 亮第一顆
+      lanterns[2].draw(); // 亮第二顆
+      lanterns[4].draw(); // 亮第三顆
+      lanterns[6].draw(); // 亮第四顆
+      lanterns[8].draw(); // 亮第五顆
+      lanterns[11].draw();
+      lanterns[13].draw();
+    }else if(rms >= _range * 6 && rms < _range * 7){
+      lanterns[0].draw(); // 亮第一顆
+      lanterns[2].draw(); // 亮第二顆
+      lanterns[4].draw(); // 亮第三顆
+      lanterns[6].draw(); // 亮第四顆
+      lanterns[8].draw(); // 亮第五顆
+      lanterns[10].draw(); // 亮第六顆
+      lanterns[13].draw();
+    }else{
+      lanterns[0].draw(); // 亮第一顆
+      lanterns[2].draw(); // 亮第二顆
+      lanterns[4].draw(); // 亮第三顆
+      lanterns[6].draw(); // 亮第四顆
+      lanterns[8].draw(); // 亮第五顆
+      lanterns[10].draw(); // 亮第六顆
+      lanterns[12].draw(); // 亮第七顆
+    }
   
+    // 祭典燈籠，以下這個順序不可變動!(因為第一次就要讓他是亮的)
+    sai_lantern_2.draw();
+    if(normal_sai_lantern_ctrl == true){
+      sai_lantern_1.draw();
+    }
+  
+    // 掛燈籠的繩子
+    drawThread();
 
+    // 攤商
+    for (var i = vendors.length-1; i >= 0; i--) {
+        vendors[i].draw();
+    }
+  
 }
 
-function mountain(){
-    noStroke();
-    fill('brown');
-	beginShape();
-	vertex(400,450);
-	vertex(500,350);
-	vertex(600,450);
-	endShape(CLOSE);
+function drawThread(){
+    // 繩子
+    var p1 = { x: 0, y: 0 };
+    var p2 = { x: 0, y: windowHeight * 0.4 }; // 起始
+    var p3 = { x: windowWidth, y: windowHeight * 0.55 }; // 終點
+    var p4 = { x: windowWidth, y: windowHeight * 0.3};
+    noFill();
+    strokeWeight(3);
+    stroke(255, 102, 0);
+    curve(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, p4.x, p4.y);
 }
-  
-function Lantern(img_path){
+
+function Vendor(img_path, x, y){
     this.image = loadImage(img_path);
-    this.width = 300;
-    this.height = 300;
-    this.x = 200;
-    this.y = 200;
+    this.width = int(windowHeight / 3);
+    this.height = int(windowHeight / 3);
+    this.x = x;
+    this.y = y;
+}
+
+Vendor.prototype.draw = function() {
+    image(this.image, this.x, this.y, this.width, this.height);
+}
+  
+function Lantern(img_path, x, y){
+    this.image = loadImage(img_path);
+    this.width = int(windowHeight / 10);
+    this.height = int(windowHeight / 10);
+    this.x = x;
+    this.y = y;
+    this.timecount = random(1, 7);
 }
 
 Lantern.prototype.draw = function() {
@@ -237,16 +370,7 @@ function building(){
     }  
 
     fill(0);
-    rect(0, windowHeight * 0.98, windowWidth, windowHeight);
-  
-  
-}
-
-
-function ground(){
-    noStroke();
-	fill('green');
-	rect(0,400,600,100);
+    rect(0, windowHeight * 0.98, windowWidth, windowHeight);  
 }
 
 function createStars(){
@@ -257,7 +381,7 @@ function createStars(){
 
 function star() {
    this.x = random(windowWidth);
-   this.y = random(windowHeight-200);
+   this.y = random(windowHeight * 0.6);
    this.size_x = int(random(1, 4));
    this.size_y = this.size_x;
    this.timeCount = int(random(0, 12));
@@ -307,8 +431,9 @@ function ShootingStar() {
   this.h = 4;
   this.color = "white";
   this.pct = 0.0;
-  this.step = 0.1;
-  this.exponent = 4;
+  this.step = 0.01;
+  this.exponent = 10;
+  this.timecount = 0;
 }
 
 ShootingStar.prototype.draw = function() {
@@ -319,7 +444,6 @@ ShootingStar.prototype.draw = function() {
   if (this.pct < 1.0 || this.x < windowWidth) {
     this.x = this.x + this.pct * windowWidth;
     this.y = this.y + pow(this.pct, this.exponent) * windowHeight;
-    console.log(this.y);    
   }else{
     shooting_stars.splice(0, 1); // 刪掉流星
   }
@@ -330,29 +454,82 @@ function starFly(i){
 }
 
 
-function moon(x, y){
-    push();
-    noStroke();
-    translate(x, y); // 位置
-    fill("#f7f25f"); // 黃色
+function Moon(x, y){
+    this.x = x;
+    this.y = y;
+}
 
-    // 繪製陰影
-    drawingContext.shadowColor = color("#f7f25f");
-    drawingContext.shadowBlur = blur_ctrl;
-    circle(moon_X, moon_Y, 60);
-    // 待畫
-    // 月亮斑點 (還沒弄好)
-    // noStroke();
-    // fill("gray");
-    // circle(moon_X + 10, moon_Y + 10, 15); 
-    // circle(moon_X - 10, moon_Y - 10, 25);
-    
-    pop();
-    
+function keyPressed() {
+  // console.log(initial_volume);
+  if (keyCode === UP_ARROW) { 
+    if(initial_volume <= 1){
+      initial_volume += 0.05;
+      if(initial_volume > 1){
+        initial_volume = 1;
+      }
+    }
+  } else if (keyCode === DOWN_ARROW) {
+    if(initial_volume >= 0){
+      initial_volume -= 0.05;
+      if(initial_volume < 0){
+        initial_volume = 0;
+      }
+    }    
+  }
+  background_music.setVolume(initial_volume);
+}
+
+function mousePressed() {
+    // 控制月亮
+    // console.log(moon.x, mouseX, moon.y, mouseY);
+    if (mouseX > (moon.x - moon_diameter / 2) & mouseX < (moon.x + moon_diameter / 2) & mouseY > (moon.y - moon_diameter / 2) & mouseY < (moon.y + moon_diameter / 2) & mouseIsPressed ) {
+      if(normal_moon_ctrl == true){
+        normal_moon_ctrl = false;
+      }else{
+        normal_moon_ctrl = true;
+      }
+    }
+  
+    // 控制祭典燈籠
+      if(normal_sai_lantern_ctrl == true){
+        if (mouseX > (sai_lantern_1.x) & mouseX < (sai_lantern_1.x + sai_lantern_1.width ) & mouseY > (sai_lantern_1.y) & mouseY < (sai_lantern_1.y + sai_lantern_1.height) & mouseIsPressed ) {
+            normal_sai_lantern_ctrl = false;
+            firework_control = false;
+            Pd.send('firework_stop', ['bang']); // 關掉煙火聲音
+          }
+      }else{
+        if (mouseX > (sai_lantern_2.x) & mouseX < (sai_lantern_2.x + sai_lantern_2.width ) & mouseY > (sai_lantern_2.y) & mouseY < (sai_lantern_2.y + sai_lantern_2.height) & mouseIsPressed ) {
+            normal_sai_lantern_ctrl = true;
+            firework_control = true;
+            fireworks = []; // 清空煙火
+          }
+      }
+}
+
+Moon.prototype.draw = function() {
+    if(normal_moon_ctrl == true){
+      push();
+      noStroke();
+      fill("#f7f25f"); // 黃色
+      // 繪製陰影
+      drawingContext.shadowColor = color("#f7f25f");
+      drawingContext.shadowBlur = blur_ctrl;
+      circle(this.x, this.y, moon_diameter);
+      pop();
+    }else{
+      push();
+      noStroke();
+      fill("black");
+      drawingContext.shadowColor = color("white");
+      drawingContext.shadowBlur = blur_ctrl;
+      circle(this.x, this.y, moon_diameter);
+      pop();
+    }
+  
     // 月亮呼吸燈
     if(blur_ctrl_flag == false){ 
         blur_ctrl = blur_ctrl + 1;
-        if(blur_ctrl == 100){
+        if(blur_ctrl == 70){
             blur_ctrl_flag = true;
         }
     }else{
@@ -363,53 +540,29 @@ function moon(x, y){
     }
 }
 
-function setGradient(x, y, w, h, c1, c2, axis) {
+function setGradient(x, y, w, h, c1, c2) {
   noFill();
-  if (axis == "Y") {  // Top to bottom gradient
-    for (let i = y; i <= y+h; i++) {
-      var inter = map(i, y, y+h, 0, 0.85);
-      var c = lerpColor(c1, c2, inter);
-      stroke(c);
-      line(x, i, x+w, i);
-    }
-  }  
-  else if (axis == "X") {  // Left to right gradient
-    for (let j = x; j <= x+w; j++) {
-      var inter2 = map(j, x, x+w, 0, 1);
-      var d = lerpColor(c1, c2, inter2);
-      stroke(d);
-      line(j, y, j, y+h);
-    }
+  for (let i = y; i <= y+h; i++) {
+    var inter = map(i, y, y+h, 0, 0.85);
+    var c = lerpColor(c1, c2, inter);
+    stroke(c);
+    line(x, i, x+w, i);
   }
 }
 
-// 傍晚 (到時候我也會改成有雲的、然後每次開啟雲都會在不同的位置)
+// 傍晚
 function eveningBackground(){ 
     var color1 = color(0, 50, 138);
     var color2 = color(255, 131, 10);
-    setGradient(0, 0, windowWidth, windowHeight, color1, color2, "Y");
+    setGradient(0, 0, windowWidth, windowHeight, color1, color2);
 }
 
-// 黑夜 (到時候我也會改成有雲的、然後每次開啟雲都會在不同的位置)
+// 黑夜
 function nightBackground(){
-    background('black');
+    var color1 = color(0, 50, 138);
+    var color2 = color(0, 0, 0);
+    setGradient(0, 0, windowWidth, windowHeight, color1, color2);
 }
-
-
-// function GetAudio() {
-//    if(Pd.getAudio() == null) {
-//     console.log("can not get audio");
-//   }
-  
-//   audioCtx = Pd.getAudio().context;
-  
-//   for(let i = 0; i < 3; i++){
-//      analysers[i] = audioCtx.createAnalyser();
-//      patch.o(i).getOutNode().connect(analysers[i]);    
-//   }
-  
-// }
-
 
 // 煙火
 function Firework() {
@@ -420,13 +573,12 @@ function Firework() {
   this.exploded = false;
   this.particles = [];
   
-  
   this.done = function() {
     if (this.exploded && this.particles.length === 0){
       return true;
-  }else {
-      return false;
-  }
+    }else {
+        return false;
+    }
   }
   this.update = function() { //firstUpdate
     if (!this.exploded) {
@@ -435,7 +587,7 @@ function Firework() {
       if (this.firework.vel.y >= 0) {
         this.exploded = true;
         this.explode();
-        Pd.send('bgm_start', ['bang']); // 爆掉的時候才要發出聲音
+        Pd.send('firework_start', ['bang']); // 爆掉的時候才要發出聲音
       }
     }
  for (var i = this.particles.length-1; i >= 0; i--) {
@@ -489,7 +641,7 @@ function Particle(x, y, hu1, hu2, hu3, firework) {
   this.update = function() { //second update
     if (!this.firework) {
       this.vel.mult(0.9);
-      this.lifespan -= 4;
+      this.lifespan -= 3;
     }
     this.vel.add(this.acc);
     this.pos.add(this.vel);
@@ -503,7 +655,7 @@ function Particle(x, y, hu1, hu2, hu3, firework) {
     }
   }
   this.show = function() {
-   colorMode(HSB);
+   colorMode(RGB);
     if (!this.firework) {
       strokeWeight(2); // 煙火爆炸的時候的粒子大小
       stroke(hu1, hu2, hu3, this.lifespan);
@@ -516,35 +668,28 @@ function Particle(x, y, hu1, hu2, hu3, firework) {
   }
 }
 
-function mountains(closerColor, furtherColor, mistColor){
+function mountains(closerColor, furtherColor){
   var y0 = width - width * 0.5; // 調整山的高度
   var i0 = 30; 
-  
+  var dx = 0;  
   var cy = [3];
-  for (var j = 0; j < 3; j++)
-  {
-    cy[2-j] = y0;
-    y0 -= i0 / pow(1.2, j);
+  
+  for (var j = 0; j < 3; j++){
+      cy[2-j] = y0;
+      y0 -= i0 / pow(1.2, j);
   }
   
-  
-  //DRAW/
-  var dx = 0;
-  
-  for (var j = 0; j <  4; j++)
-  {                  
-    for (var x = 0; x < width; x ++)
-    {          
+  for (var j = 0; j <  4; j++){                  
+    for (var x = 0; x < width; x ++){          
       var y = cy[j]; //y = reference y
-      y += d*(j+6)*noise(1.2*dx/(j+6)+e);  //adds randomness to the mountains
-      y += 1.7*(j+6)*noise(10*dx);  //adds trees
+      y += d * (j+6) * noise(1.2*dx/(j+6)+e);
+      y += 1.7 * (j+6) * noise(10*dx);
       
-      strokeWeight(2);  //smoove peaks
+      strokeWeight(2); 
       stroke(lerpColor(furtherColor, closerColor, (j+6)/10));
       line(x, y, x, windowHeight); 
       
       dx += 0.02;
-    }
-    
+    }    
   }
 }
